99b4003798617437a86f65c14d109f4882e227c2
omar Kadahgit config --global user.email omarmkadah@gmail.com <macowner@Macs-iMac.local>
2019-01-16 22:20:21 -0800

    completed interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,43 @@
-var add = function(num1, num2){
-	return num1 + num2;
-}
+// Below is the Back-End Logic for the calculator:
 
-function add (num1, num2){
-	return num1 + num2;
-}
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
-function subtract(num1, num2){
-	return num1 - num2;
-}
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-function multiply (num1, num2){
-	return num1 * num2;
-}
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-function divide (num1, num2){
-	return num1 / num2;
-}
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-var num1 = parseInt(prompt("Enter a Number"));
-var num2 = parseInt(prompt("Enter a second number"));
 
-result = add(num1, num2);
-result4 = subtract(num1, num2);
-result2 = multiply(num1, num2);
-result3 = divide(num1, num2);
+// result = add(num1, num2);
+// result4 = subtract(num1, num2);
+// result2 = multiply(num1, num2);
+// result3 = divide(num1, num2);
 
 // alert(result);
 // alert(result4);
 // alert(result2);
 // alert(result3);
 
+// The below was an alert for previous assignment
+// alert("Addition: " + result + " Subtract: " + result4 + " Multiply: " + result2 + " Divide: " + result3);
 
-alert("Addition: " + result + " Subtract: " + result4 + " Multiply: " + result2 + " Divide: " + result3);
+// Code below is for front end logic (User-Interace Logic):
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
